e25108562f3e0da1c764887cb59ca75a1eca1eb0
Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>
2018-02-21 14:53:53 -0800

    Use Homebrew formula for Sierra and lower to use a more recent TLS library version (supports TLS 1.2)
    Signed-off-by: Nassim 'Nass' Eddequiouaq <eddequiouaq.nassim@gmail.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -14,7 +14,10 @@ class DockerCompose < Formula
     sha256 "61b27a9bbed711d265f8eb3d598bf25c4b86bfbff8aab4fe02d0c0039904e97a" => :el_capitan
   end
 
-  depends_on "python" if MacOS.version <= :snow_leopard
+  # Use Homebrew's Python because MacOS versions below High Sierra link against old OpenSSL versions
+  # High Sierra's links against LibreSSL 2.2.7
+  depends_on "python" if MacOS.version <= :sierra
+
   depends_on "libyaml"
 
   # It's possible that the user wants to manually install Docker and Machine,
